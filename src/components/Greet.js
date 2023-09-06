dc5bae423607e9e3a93c7b8568a3ccbff764a51d
Tracy-miranja <barazatracy44@gmail.com>
2023-09-06 15:17:51 +0300

    Add another component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Greet(){
-    return <h1>Hello kushie Tracy</h1>
+// function Greet() {
+//     return <h1>Hello kushie Tracy</h1>
+// }
+
+//ES6
+const Greet = () =>{
+    return <h1>Hello KUSHIE</h1>
 }
 
-export default Greet();
+export default Greet;
